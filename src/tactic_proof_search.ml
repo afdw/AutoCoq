b77958ac44f7ed278d39350dd0f4fae587b1fcd8
Anton Danilkin <afdw@yandex.ru>
2024-06-09 20:04:45 +0200

    Use probabilities returned from model

--- src/tactic_proof_search.ml
+++ src/tactic_proof_search.ml
@@ -3,7 +3,7 @@ open Proofview.Notations
 type infer_request = string
   [@@deriving yojson { variants = `Internal "type"; exn = true }]
 
-type infer_response = string list
+type infer_response = (string * float) list
   [@@deriving yojson { variants = `Internal "type"; exn = true }]
 
 let http_infer (url : string) (infer_request : infer_request) : infer_response =
@@ -83,6 +83,20 @@ let rec simulate_proofstates : type a. (proofstate * a) list -> a Proofview.tact
       (load_proofstate proofstate <*> Proofview.tclUNIT r)
       (fun _ -> simulate_proofstates proofstates)
 
+let if_solved (type a b) (tac : a Proofview.tactic) (then_ : a -> b Proofview.tactic) (else_ : a Proofview.tactic -> b Proofview.tactic) =
+  capture_proofstates tac >>= fun proofstates ->
+  let solution_proofstate =
+    proofstates |> List.find_opt (fun (proofstate, _) ->
+      let (_, _, goals) = proofstate in
+      goals = []
+    ) in
+  match solution_proofstate with
+  | Some (proofstate, x) ->
+    load_proofstate proofstate <*>
+    then_ x
+  | None ->
+    else_ (simulate_proofstates proofstates)
+
 let eager (type a key) ?(compare : key -> key -> int = Stdlib.compare) (measure : key Proofview.tactic) (tac : a Proofview.tactic) : a Proofview.tactic =
   let t =
     tac >>= fun r ->
@@ -133,29 +147,96 @@ let print_goal (goal : Proofview.Goal.t) : string =
   ) |> String.concat ", ") ^ " ⊢ " ^
   simple_string_of_ppcmds (Printer.pr_econstr_env env sigma (goal |> Proofview.Goal.concl))
 
+let f_branch_proba : float Evd.Store.field = Evd.Store.field "branch_proba"
+
+let get_branch_proba : float Proofview.tactic =
+  Proofview.tclEVARMAP >>= fun sigma ->
+  let store = Evd.get_extra_data sigma in
+  let branch_proba = Evd.Store.get store f_branch_proba |> Option.default 1.0 in
+  Proofview.tclUNIT branch_proba
+
+let update_branch_proba (proba_multiplier : float) : unit Proofview.tactic =
+  Proofview.tclEVARMAP >>= fun sigma ->
+  let store = Evd.get_extra_data sigma in
+  let branch_proba = Evd.Store.get store f_branch_proba |> Option.default 1.0 in
+  let branch_proba = branch_proba *. proba_multiplier in
+  let store = Evd.Store.set store f_branch_proba branch_proba in
+  let sigma = Evd.set_extra_data store sigma in
+  Proofview.Unsafe.tclEVARS sigma
+
+let f_trace : string list Evd.Store.field = Evd.Store.field "trace"
+
+let get_trace : string list Proofview.tactic =
+  Proofview.tclEVARMAP >>= fun sigma ->
+  let store = Evd.get_extra_data sigma in
+  let trace = Evd.Store.get store f_trace |> Option.default [] in
+  Proofview.tclUNIT trace
+
+let cons_trace (trace_element : string) : unit Proofview.tactic =
+  Proofview.tclEVARMAP >>= fun sigma ->
+  let store = Evd.get_extra_data sigma in
+  let trace = Evd.Store.get store f_trace |> Option.default [] in
+  let trace = trace @ [trace_element] in
+  let store = Evd.Store.set store f_trace trace in
+  let sigma = Evd.set_extra_data store sigma in
+  Proofview.Unsafe.tclEVARS sigma
+
+exception DepthLimitExceeded
+
 let proof_search (infer : infer_request -> infer_response) : unit Proofview.tactic =
-  let step =
+  let step current_depth =
     Proofview.tclFOCUS 1 1 (
       Proofview.Goal.enter (fun goal ->
+        Feedback.msg_info Pp.(str "Current depth: " ++ int current_depth);
         let printed_goal = print_goal goal in
-        Feedback.msg_info Pp.(str "Goal goal: " ++ str printed_goal);
-        let t = or_many (infer printed_goal |> List.map (fun s ->
+        get_trace >>= fun trace ->
+        Feedback.msg_info Pp.(str "In trace [" ++ (trace |> prlist_with_sep (fun () -> str "; ") str) ++ str "], got goal: " ++ str printed_goal);
+        or_many (infer printed_goal |> List.map (fun (s, proba) ->
           Proofview.tclUNIT () >>= fun () ->
           Feedback.msg_info Pp.(str "Applying: " ++ str s);
-          Proofview.tclPROGRESS (run_ltac s)
-        )) in
-        eager Proofview.numgoals t
+          update_branch_proba proba <*>
+          cons_trace s <*>
+          Proofview.tclTIMEOUTF 0.1 (Proofview.tclPROGRESS (run_ltac s)) <*>
+          Proofview.numgoals >>= fun goal_count ->
+          Feedback.msg_info Pp.(str "  -> " ++ int goal_count ++ str " goals");
+          Proofview.tclUNIT ()
+        ))
       )
     ) in
-  let rec search current_depth =
-    Feedback.msg_info Pp.(str "Current depth: " ++ int current_depth);
-    Proofview.numgoals >>= fun goal_count ->
-    if goal_count = 0 then
-      Proofview.tclUNIT ()
-    else
-      step >>= fun () ->
-      search (current_depth + 1) in
-  search 0
+  let rec search current_depth t =
+    if_solved
+      t
+      (fun () ->
+        get_trace >>= fun trace ->
+        Feedback.msg_info Pp.(str "Proved! Trace: [" ++ (trace |> prlist_with_sep (fun () -> str "; ") str) ++ str "]");
+        Proofview.tclUNIT ()
+      )
+      (fun t ->
+        search (current_depth + 1) (
+          let measure =
+            Proofview.numgoals >>= fun goal_count ->
+            get_branch_proba >>= fun branch_proba ->
+            Proofview.tclUNIT ((1.0 -. branch_proba) *. 0.8 +. (float_of_int goal_count) *. 0.2) in
+          eager measure t <*>
+          step current_depth
+        )
+      ) in
+  search 0 (Proofview.tclUNIT ())
+  (*
+    eager Proofview.numgoals (
+      eager Proofview.numgoals (
+        eager Proofview.numgoals (
+          eager Proofview.numgoals (
+            Proofview.tclUNIT ()
+          ) <*>
+          step 1
+        ) <*>
+        step 2
+      ) <*>
+      step 3
+    ) <*>
+    Proofview.tclINDEPENDENT (Proofview.tclZERO ~info:(Exninfo.reify()) DepthLimitExceeded) in
+  *)
 
 let entry_tactic_proof_search (url : string) : unit Proofview.tactic =
   proof_search (http_infer url)
